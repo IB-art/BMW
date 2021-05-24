cb1dc3c360c919986acbe5f6a631126f355d77c8
IB-art <65600199+IB-art@users.noreply.github.com>
2021-05-24 18:10:35 +0200

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,35 @@
+import blockScrolled from './blockScrolled.js';
+
+const {disabledScroll, enableScroll} = blockScrolled;
+
+export default function modal() {
+  const designBlockElem = document.querySelector('.design-block');
+  const modalElem = document.querySelector('.modal');
+
+  const openModal = () => {
+    modalElem.classList.remove('hidden');
+    disabledScroll();
+  };
+ 
+  const closeModal = () => {
+   modalElem.classList.add('hidden');
+   enableScroll();  
+  };
+
+  designBlockElem.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.matches('.more')) {
+      openModal();
+    }
+  });
+
+  modalElem.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('overlay') ||
+       target.classList.contains('modal__close')) {
+       closeModal() 
+    } 
+  })
+}
---
+++ js/sendForm.js
@@ -0,0 +1,87 @@
+const server = 'https://jsonplaceholder.typicode.com/posts';
+
+const sendData = (data, callBack, falseCallBack) => {
+  const request = new XMLHttpRequest();
+  request.open('POST', server);
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState !== 4) return;
+    if (request.status === 200 || request.status === 201) {
+      const response = JSON.parse(request.responseText);
+      callBack(response.id);
+    } else {
+      // falseCallBack(request.responseText)
+      // throw new Error(request.statusText)
+      falseCallBack(request.status)
+      throw new Error(request.status)
+    }
+  }); 
+
+  request.send(data)
+};
+
+
+const formHandler = (form) => {
+  const smallElem = document.createElement('small');
+  form.append(smallElem);
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = {};
+    let flag = true;
+
+    const buttonSubmit = form.querySelector('.button[type="submit"]');  
+
+    for (const elem of form.elements) {
+      const {name, value} = elem;
+      if (name){
+        if (value.trim()) {
+          elem.style.border = '';
+          data[name] = value.trim();
+          // flag = true
+        } else {
+          elem.style.border = '1px solid red';
+          flag = false;
+          elem.value = '';
+          // return smallElem.textContent = 'Заполните все поля';
+        }
+      }
+    }
+
+    if (!flag) {
+      return smallElem.textContent = 'Заполните все поля';
+    }
+
+    // const smallElem = document.createElement('small');
+
+    sendData(JSON.stringify(data), 
+    (id) => {
+      // smallElem.textContent = 'Ваша заявка №' + id + '! \nВ ближайшее время с вами свяжемся!';
+      smallElem.innerHTML = 'Ваша заявка №' + id + '!<br> В ближайшее время с вами свяжемся!';
+      smallElem.style.color = 'green';
+      buttonSubmit.disabled = true;
+
+      setTimeout(() => {
+        smallElem.textContent = '';
+        buttonSubmit.disabled = false;
+      }, 5000)
+    }, 
+    (err) => {
+      smallElem.textContent = 'К сожалению технические неполадки, попробуйте отправить заявку позже';
+      smallElem.style.color = 'red';
+     
+    });
+    form.reset();
+  })
+};
+
+export default function sendForm() {
+
+  const formElems = document.querySelectorAll('.form'); 
+  
+  formElems.forEach(formHandler)
+
+}
+
+
+
+ 
